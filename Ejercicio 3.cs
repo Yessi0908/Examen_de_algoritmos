2a70b420d62401ca139b3817cb47ad083604b5d8
Yessi0908 <jramirezs26@miumg.edu.gt>
2022-11-12 16:11:01 -0600

    first commit

---
+++ Ejercicio 3.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.taks;
+namespace Ejercicio3
+{class Ejercicio3
+    { private int[,] mt2;
+        public void Inicia()
+        {mt2 = new int[3, 5];
+         for (int f = 0; f < 3; f++)
+        { for (int c = 0; c < 5; c++)
+             {Console.Write("Ingrese posicion ["+(f+1)+","+(c+1)+"]: ");
+             string linea;
+            linea = Console.ReadLine();
+            mt2[f, c] = int.Parse(linea); }
+            }
+        }
+
+        public void sale()
+        {for (int f = 0; f < 3; f++)
+            {for (int c = 0; c < 5; c++)
+         {  Console.Write(mt2[f, c] + " "); }
+            Console.WriteLine();}
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {Matriz ma = new Matriz();
+            ma.Inicia();
+            ma.sale();}
+    }
+}
---
+++ ejercicio 2;.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.taks;
+namespace ejercicio2
+{class ejercicio2
+    {  private int[,] mt;
+    public void inicio()
+        {
+            mt = new int[3, 4];
+            for (int f = 0; f < 3; f++)
+            {for (int c = 0; c < 4; c++)
+                {Console.Writeline("Ingrese un número ["+(f+1)+","+(c+1)+"]: ");
+                    string linea;
+                    linea = Console.ReadLine();
+                    mt[f, c] = int.Parse(linea); }
+            }
+        }
+        public void nFila()
+        {Console.WriteLine("\nPrimer fila:");
+            for (int c = 0; c < 4; c++)
+            { Console.Write(mt[0, c]+"  ");}
+        }
+        public void lfila()
+        {Console.WriteLine("\nUltima fila:");
+            for (int c = 0; c < 4; c++)
+            { Console.Write(mt[2, c]+"  ");}
+        }
+
+        public void ucolumna()
+        {
+            Console.WriteLine("\nUltima columna:");
+            for (int f = 0; f < 3; f++)
+            {Console.Write(mt[f, 0]+"  "); }
+        }
+
+        static void Main(string[] args)
+        {ejercicio2 ma = new ejercicio2();
+            ma.inicio();
+            ma.nFila();
+            ma.lfila();
+            ma.ucolumna();
+            Console.ReadKey();
+        }
+    }
+}
